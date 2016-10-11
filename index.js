9464d722d86d53721d2914e1e223340eddcd2478
Arthur Stolyar <nekr.fabula@gmail.com>
2016-10-11 19:12:10 +0300

    Update to support inline globs

--- index.js
+++ index.js
@@ -2,23 +2,67 @@
 
 var glob = require("glob");
 var path = require("path");
+var loaderUtils = require("loader-utils");
 
 module.exports = function (content, sourceMap) {
   this.cacheable && this.cacheable();
-  var resourceDir = path.dirname(this.resourcePath);
-  var pattern = content.trim();
-  var files = glob.sync(pattern, {
+
+  var query = loaderUtils.parseQuery(this.query);
+
+  if (!query.pattern) {
+    var resourceDir = path.dirname(this.resourcePath);
+    query = Object.keys(query).reduce(function(result, key) {
+      result[key] = query[key];
+      return result;
+    }, {});
+
+    query.pattern = content.trim();
+    return handle.call(this, query, resourceDir);
+  } else {
+    return content;
+  }
+};
+
+
+// var request = loaderUtils.stringifyRequest(this, remainingRequest);
+
+module.exports.pitch = function(remainingRequest, precedingRequest, data) {
+  this.cacheable && this.cacheable();
+
+  var query = loaderUtils.parseQuery(this.query);
+  var resourceDir;
+
+  if (query.pattern) {
+    var issuer = this._module.issuer;
+    var issuerIndex = issuer.lastIndexOf('!');
+
+    if (issuerIndex !== -1) {
+      issuer = issuer.slice(issuerIndex + 1);
+    }
+
+    return handle.call(this, query, issuer);
+  }
+};
+
+function handle(query, issuer) {
+  var resourceDir = path.dirname(issuer);
+
+  var files = glob.sync(query.pattern, {
     cwd: resourceDir
   });
 
   if (!files.length) {
-    this.emitWarning('Did not find anything for glob "' + pattern + '" in directory "' + resourceDir + '"');
+    this.emitWarning('Did not find anything for glob "' + query.pattern + '" in directory "' + resourceDir + '"');
   }
 
-  return "module.exports = {\n" + files.map(function (file) {
-    this.addDependency(path.resolve(resourceDir, file));
+  var result = "module.exports = {\n" + files.map(function (file) {
+    var dep = path.resolve(resourceDir, file);
+    this.addDependency(dep);
 
     var stringifiedFile = JSON.stringify(file);
-    return "\t" + stringifiedFile + ": require(" + stringifiedFile + ")";
+    var stringifiedDep = JSON.stringify(dep);
+    return "\t" + stringifiedFile + ": require(" + stringifiedDep + ")";
   }, this).join(",\n") + "\n};"
-};
+
+  return result;
+}
